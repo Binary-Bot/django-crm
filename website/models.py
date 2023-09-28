2b464d4de07d8edbd00e9adf0fad372bd24e04e5
Shashwat Rajkarnikar <shashwatraj2000@gmail.com>
2023-09-27 19:19:52 -0500

    added the function to delete records

--- website/models.py
+++ website/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Record(models.Model):
-    year = models.IntegerField(max_length=4)
+    year = models.IntegerField()
     attorney = models.CharField(max_length=50)
     client_description = models.CharField(max_length=500)
     matter_desc = models.CharField(max_length=500)
     matter_only = models.BooleanField(max_length=2)
-    client_num = models.IntegerField(max_length=10)
-    matter_num = models.IntegerField(max_length=10)
+    client_num = models.IntegerField()
+    matter_num = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.CharField(max_length=50)
 
--- website/views.py
+++ website/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 # Create your views here.
 def home(request):
     # Check to see if logging in
+    records = Record.objects.all()
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -20,7 +23,7 @@ def home(request):
             messages.success(request, "There was an error logging you in")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 def login_user(request):
@@ -49,6 +52,32 @@ def register_user(request):
             return redirect('home')
     else:
         form = SignUpForm()
-        return render(request, 'register.html', {'form':form})
+        return render(request, 'register.html', {'form': form})
 
-    return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form': form})
+
+
+# View a case record by id
+def case_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'case_record': case_record})
+    else:
+        messages.success(request, "You must be logged in to view that page!")
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Deleted Successfully...")
+        return redirect('home')
+    else:
+        messages.success(request, "You Must Be Logged In To Do That...")
+        return redirect('home')
+
+
+def add_record(request):
+    return render(request, 'add_record.html', {'case_record': case_record})
+
